1ea681e931b80f95249d72cc0b1c0d86793975f6
Zephyra77 <dibomaharani18@gmail.com>
2025-10-20 13:33:33 +0700

    Fix

--- OtakudesuProvider/src/main/kotlin/com/hexated/OtakudesuProvider.kt
+++ OtakudesuProvider/src/main/kotlin/com/hexated/OtakudesuProvider.kt
@@ -59,9 +59,6 @@ class OtakudesuProvider : MainAPI() {
         val title = this.selectFirst("h2.jdlflm")?.text()?.trim() ?: return null
         val href = this.selectFirst("a")!!.attr("href")
         val posterUrl = this.select("div.thumbz > img").attr("src").toString()
-
-        val epNum = this.selectFirst("div.epz")?.ownText()?.replace(Regex("\\D"), "")?.trim()?.toIntOrNull()
-
         val epNum = this.selectFirst("div.epz")?.ownText()?.replace(Regex("\\D"), "")?.toIntOrNull()
 
         return newAnimeSearchResponse(title, href, TvType.Anime) {
@@ -71,14 +68,6 @@ class OtakudesuProvider : MainAPI() {
     }
 
     override suspend fun search(query: String): List<SearchResponse> {
-
-        return app.get("$mainUrl/?s=$query&post_type=anime").document.select("ul.chivsrc > li").map {
-            val title = it.selectFirst("h2 > a")!!.ownText().trim()
-            val href = it.selectFirst("h2 > a")!!.attr("href")
-            val posterUrl = it.selectFirst("img")!!.attr("src").toString()
-            newAnimeSearchResponse(title, href, TvType.Anime) { this.posterUrl = posterUrl }
-        }
-
         return app.get("$mainUrl/?s=$query&post_type=anime").document.select("ul.chivsrc > li")
             .mapNotNull {
                 val title = it.selectFirst("h2 > a")?.ownText()?.trim() ?: return@mapNotNull null
@@ -86,7 +75,6 @@ class OtakudesuProvider : MainAPI() {
                 val posterUrl = it.selectFirst("img")!!.attr("src").toString()
                 newAnimeSearchResponse(title, href, TvType.Anime) { this.posterUrl = posterUrl }
             }
-
     }
 
     override suspend fun load(url: String): LoadResponse {
@@ -95,32 +83,23 @@ class OtakudesuProvider : MainAPI() {
         val poster = document.selectFirst("div.fotoanime > img")?.attr("src")
         val tags = document.select("div.infozingle > p:nth-child(11) > span > a").map { it.text() }
         val type = getType(document.selectFirst("div.infozingle > p:nth-child(5) > span")?.ownText()?.replace(":", "")?.trim() ?: "tv")
-
-        val year = Regex("\\d, (\\d*)").find(document.select("div.infozingle > p:nth-child(9) > span").text())?.groupValues?.get(1)?.toIntOrNull()
-
         val year = Regex("\\d{4}").find(document.select("div.infozingle > p:nth-child(9) > span").text())?.value?.toIntOrNull()
-
         val status = getStatus(document.selectFirst("div.infozingle > p:nth-child(6) > span")!!.ownText().replace(":", "").trim())
         val description = document.select("div.sinopc > p").text()
+
         val episodes = document.select("div.episodelist")[1].select("ul > li").mapNotNull {
             val name = it.selectFirst("a")?.text() ?: return@mapNotNull null
-
-            val episode = Regex("Episode\\s?(\\d+)").find(name)?.groupValues?.getOrNull(0) ?: it.selectFirst("a")?.text()
-
             val episode = Regex("Episode\\s?(\\d+)").find(name)?.groupValues?.getOrNull(1)?.toIntOrNull()
-
             val link = fixUrl(it.selectFirst("a")!!.attr("href"))
             newEpisode(link) { this.episode = episode }
         }.reversed()
+
         val recommendations = document.select("div.isi-recommend-anime-series > div.isi-konten").map {
             val recName = it.selectFirst("span.judul-anime > a")!!.text()
             val recHref = it.selectFirst("a")!!.attr("href")
             val recPosterUrl = it.selectFirst("a > img")?.attr("src").toString()
             newAnimeSearchResponse(recName, recHref, TvType.Anime) { this.posterUrl = recPosterUrl }
         }
-
-        val tracker = APIHolder.getTracker(listOf(title), TrackerType.getTypes(type), year, true)
-
 
         return newAnimeLoadResponse(title, url, type) {
             posterUrl = poster
@@ -133,106 +112,88 @@ class OtakudesuProvider : MainAPI() {
         }
     }
 
-    data class ResponseSources(@JsonProperty("id") val id: String, @JsonProperty("i") val i: String, @JsonProperty("q") val q: String)
+    data class ResponseSources(
+        @JsonProperty("id") val id: String,
+        @JsonProperty("i") val i: String,
+        @JsonProperty("q") val q: String
+    )
+
     data class ResponseData(@JsonProperty("data") val data: String)
 
-    override suspend fun loadLinks(data: String, isCasting: Boolean, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit): Boolean {
+    override suspend fun loadLinks(
+        data: String,
+        isCasting: Boolean,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ): Boolean {
         val document = app.get(data).document
+        val mirrorData = document.select("div.mirrorstream > ul > li").mapNotNull {
+            base64Decode(it.select("a").attr("data-content"))
+        }
 
-        val mirrorData = document.select("div.mirrorstream > ul > li").mapNotNull { base64Decode(it.select("a").attr("data-content")) }
         for (resJson in mirrorData) {
             val resList = tryParseJson<List<ResponseSources>>(resJson)
             resList?.forEach { res ->
-                val sources = Jsoup.parse(base64Decode(app.post("${mainUrl}/wp-admin/admin-ajax.php", data = mapOf(
-                    "id" to res.id,
-                    "i" to res.i,
-                    "q" to res.q,
-                    "nonce" to "",
-                    "action" to ""
-                )).parsed<ResponseData>().data)).select("iframe").attr("src")
+                val sources = Jsoup.parse(
+                    base64Decode(
+                        app.post(
+                            "$mainUrl/wp-admin/admin-ajax.php",
+                            data = mapOf("id" to res.id, "i" to res.i, "q" to res.q)
+                        ).parsed<ResponseData>().data
+                    )
+                ).select("iframe").attr("src")
                 loadCustomExtractor(sources, data, subtitleCallback, callback, getQuality(res.q))
             }
         }
+
         document.select("div.download li").forEach { ele ->
             val quality = getQuality(ele.select("strong").text())
-            ele.select("a").map { it.attr("href") to it.text() }.filter { !inBlacklist(it.first) && quality != Qualities.P360.value }.forEach {
-                val link = app.get(it.first, referer = "$mainUrl/").url
-                loadCustomExtractor(fixedIframe(link), data, subtitleCallback, callback, quality)
-            }
+            ele.select("a").map { it.attr("href") }
+                .filter { !inBlacklist(it) && quality != Qualities.P360.value }
+                .forEach {
+                    val link = app.get(it, referer = "$mainUrl/").url
+                    loadCustomExtractor(fixedIframe(link), data, subtitleCallback, callback, quality)
+                }
         }
-
-        argamap({
-            val scriptData = document.select("script:containsData(action:)").lastOrNull()?.data()
-            val token = scriptData?.substringAfter("{action:\"")?.substringBefore("\"}").toString()
-            val nonce = app.post("$mainUrl/wp-admin/admin-ajax.php", data = mapOf("action" to token)).parsed<ResponseData>().data
-            val action = scriptData?.substringAfter(",action:\"")?.substringBefore("\"}").toString()
-            val mirrorData = document.select("div.mirrorstream > ul > li").mapNotNull { base64Decode(it.select("a").attr("data-content")) }.toString()
-            tryParseJson<List<ResponseSources>>(mirrorData)?.apmap { res ->
-                val id = res.id
-                val i = res.i
-                val q = res.q
-                val sources = Jsoup.parse(base64Decode(app.post("${mainUrl}/wp-admin/admin-ajax.php", data = mapOf("id" to id, "i" to i, "q" to q, "nonce" to nonce, "action" to action)).parsed<ResponseData>().data)).select("iframe").attr("src")
-                loadCustomExtractor(sources, data, subtitleCallback, callback, getQuality(q))
-            }
-        }, {
-            document.select("div.download li").map { ele ->
-                val quality = getQuality(ele.select("strong").text())
-                ele.select("a").map { it.attr("href") to it.text() }
-                    .filter { !inBlacklist(it.first) && quality != Qualities.P360.value }
-                    .apmap {
-                        val link = app.get(it.first, referer = "$mainUrl/").url
-                        loadCustomExtractor(fixedIframe(link), data, subtitleCallback, callback, quality)
-                    }
-            }
-        })
 
         return true
     }
 
-    private suspend fun loadCustomExtractor(url: String, referer: String? = null, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit, quality: Int = Qualities.Unknown.value) {
+    private suspend fun loadCustomExtractor(
+        url: String,
+        referer: String? = null,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit,
+        quality: Int = Qualities.Unknown.value
+    ) {
         loadExtractor(url, referer, subtitleCallback) { link ->
             runBlocking {
-                callback.invoke(newExtractorLink(link.name, link.name, link.url, link.type) {
-                    this.referer = link.referer
-                    this.quality = quality
-                    this.headers = link.headers
-                    this.extractorData = link.extractorData
-                })
+                callback.invoke(
+                    newExtractorLink(link.name, link.name, link.url, link.type) {
+                        this.referer = link.referer
+                        this.quality = quality
+                        this.headers = link.headers
+                        this.extractorData = link.extractorData
+                    }
+                )
             }
         }
     }
 
     private fun fixedIframe(url: String): String {
-
-        return when {
-            url.startsWith(acefile) -> {
-                val id = Regex("""(?:/f/|/file/)(\w+)""").find(url)?.groupValues?.getOrNull(1)
-                "${acefile}/player/$id"
-            }
-            else -> fixUrl(url)
-        }
-    }
-
-    private fun inBlacklist(host: String?): Boolean {
-        return mirrorBlackList.any { it.equals(host, true) }
-    }
-
-    private fun getQuality(str: String?): Int {
-        return Regex("(\\d{3,4})[pP]").find(str ?: "")?.groupValues?.getOrNull(1)?.toIntOrNull() ?: Qualities.Unknown.value
-    }
-
         return if (url.startsWith(acefile)) {
             val id = Regex("""(?:/f/|/file/)(\w+)""").find(url)?.groupValues?.getOrNull(1)
-            "${acefile}/player/$id"
+            "$acefile/player/$id"
         } else fixUrl(url)
     }
 
     private fun inBlacklist(host: String?): Boolean = mirrorBlackList.any { it.equals(host, true) }
-    private fun getQuality(str: String?): Int = Regex("(\\d{3,4})[pP]").find(str ?: "")?.groupValues?.getOrNull(1)?.toIntOrNull() ?: Qualities.Unknown.value
 
+    private fun getQuality(str: String?): Int =
+        Regex("(\\d{3,4})[pP]").find(str ?: "")?.groupValues?.getOrNull(1)?.toIntOrNull()
+            ?: Qualities.Unknown.value
 }
 
-class Odvidhide
 class Moedesu : JWPlayer() {
     override val name = "Moedesu"
     override val mainUrl = "https://desustream.me/moedesu/"
