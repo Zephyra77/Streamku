056e56bf9b4e60b4439a85b3ba3b4beaf55202ae
Zephyra <dibomaharani18@gmail.com>
2025-10-21 02:55:39 +0700

    Fix

--- KlikxxiProvider/src/main/kotlin/com/klikxxi/Klikxxi.kt
+++ KlikxxiProvider/src/main/kotlin/com/klikxxi/Klikxxi.kt
@@ -121,7 +121,7 @@ open class Klikxxi : MainAPI() {
                 this.plot = description
                 this.tags = tags
                 this.year = year
-                this.score = rating   
+                this.score = rating?.let { Score(it.toDouble()) }  
                 addActors(actors)
                 this.recommendations = recommendations
                 addTrailer(trailer)
@@ -132,7 +132,7 @@ open class Klikxxi : MainAPI() {
                 this.plot = description
                 this.tags = tags
                 this.year = year
-                this.score = rating   
+                this.score = rating?.let { Score(it.toDouble()) }  
                 addActors(actors)
                 addTrailer(trailer)
                 this.recommendations = recommendations
