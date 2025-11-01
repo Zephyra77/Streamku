80111b9b1b8913ddc2e54a3e42fcffeb3eac2056
Zephyra <dibomaharani18@gmail.com>
2025-11-01 17:50:16 +0700

    Fix

--- Dramaindo/src/main/kotlin/com/dramaindo/DramaindoPlugin.kt
+++ Dramaindo/src/main/kotlin/com/dramaindo/DramaindoPlugin.kt
@@ -3,8 +3,6 @@ package com.dramaindo
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
-import com.dramaindo.extractors.Mitedrive
-import com.dramaindo.extractors.Berkasdrive
 
 @CloudstreamPlugin
 class DramaindoPlugin : Plugin() {
