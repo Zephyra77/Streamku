a5948f5a8ef2b2fd281aeb502fe052b9c0b40d5e
Zephyra <dibomaharani18@gmail.com>
2025-11-02 10:16:21 +0700

    Fix Extractors

--- Dramaindo/src/main/kotlin/com/dramaindo/Extractors.kt
+++ Dramaindo/src/main/kotlin/com/dramaindo/Extractors.kt
@@ -3,13 +3,14 @@ package com.dramaindo
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
-import kotlinx.coroutines.runBlocking
-import org.jsoup.select.Elements
 
 open class Mitedrive : ExtractorApi() {
     override val name = "Mitedrive"
     override val mainUrl = "https://mitedrive.com"
     override val requiresReferer = false
+
+    data class Data(@JsonProperty("original_url") val url: String? = null)
+    data class Responses(@JsonProperty("data") val data: Data? = null)
 
     override suspend fun getUrl(
         url: String,
@@ -25,15 +26,19 @@ open class Mitedrive : ExtractorApi() {
                 "csrf_token" to "ZXlKcGNDSTZJak0yTGpneExqWTFMakUyTWlJc0ltUmxkbWxqWlNJNklrMXZlbWxzYkdFdk5TNHdJQ2hYYVc1a2IzZHpJRTVVSURFd0xqQTdJRmRwYmpZME95QjROalE3SUhKMk9qRXdNUzR3S1NCSFpXTnJieTh5TURFd01ERXdNU0JHYVhKbFptOTRMekV3TVM0d0lpd2lZbkp2ZDNObGNpSTZJazF2ZW1sc2JHRWlMQ0pqYjI5cmFXVWlPaUlpTENKeVpXWmxjbkpsY2lJNklpSjk=",
                 "slug" to id
             )
-        ).parsedSafe<Responses>()?.data?.url
+        ).parsedSafe<Responses>()?.data?.url ?: return
 
-        callback.invoke(
-            newExtractorLink(name, name, video ?: return, INFER_TYPE) { this.referer = "$mainUrl/" }
+        callback(
+            newExtractorLink(
+                name,
+                name,
+                video,
+                INFER_TYPE
+            ) {
+                this.referer = "$mainUrl/"
+            }
         )
     }
-
-    data class Data(@JsonProperty("original_url") val url: String? = null)
-    data class Responses(@JsonProperty("data") val data: Data? = null)
 }
 
 open class Berkasdrive : ExtractorApi() {
@@ -47,9 +52,41 @@ open class Berkasdrive : ExtractorApi() {
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
     ) {
-        val video = app.get(url, referer = referer).document.select("video#player source").attr("src")
-        callback.invoke(
-            newExtractorLink(name, name, video, INFER_TYPE) { this.referer = "$mainUrl/" }
-        )
+        val doc = app.get(url, referer = referer).document
+        val buttons = doc.select("a.btn.btn-primary.btn-block")
+
+        if (buttons.isNotEmpty()) {
+            buttons.forEach { btn ->
+                val href = btn.attr("href")
+                val quality = Regex("(\\d{3,4})p").find(btn.text())?.groupValues?.getOrNull(1)?.toIntOrNull()
+                    ?: Qualities.Unknown.value
+
+                callback(
+                    ExtractorLink(
+                        name,
+                        "$name ${btn.text()}",
+                        href,
+                        url,
+                        quality,
+                        INFER_TYPE
+                    )
+                )
+            }
+            return
+        }
+
+        val src = doc.select("video#player source").attr("src")
+        if (src.isNotBlank()) {
+            callback(
+                ExtractorLink(
+                    name,
+                    "$name Default",
+                    src,
+                    url,
+                    Qualities.Unknown.value,
+                    INFER_TYPE
+                )
+            )
+        }
     }
 }
