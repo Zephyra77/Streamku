24911c7b9d4a3a22de1de1279c544681f4fd4e87
Zephyra <dibomaharani18@gmail.com>
2025-11-04 23:06:26 +0700

    fix

--- NontonAnimeID/src/main/kotlin/com/nontonanimeid/NontonAnimeID.kt
+++ NontonAnimeID/src/main/kotlin/com/nontonanimeid/NontonAnimeID.kt
@@ -5,8 +5,10 @@ import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.LoadResponse.Companion.addAniListId
 import com.lagradost.cloudstream3.LoadResponse.Companion.addMalId
 import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
+import com.lagradost.cloudstream3.utils.AppUtils.parallelMap
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.loadExtractor
+import kotlinx.coroutines.runBlocking
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 import java.net.URI
@@ -106,11 +108,12 @@ class NontonAnimeID : MainAPI() {
         val year = Regex("\\d, (\\d*)").find(
             document.select(".bottomtitle > span:nth-child(5)").text()
         )?.groupValues?.get(1)?.toIntOrNull()
+
         val status = getStatus(
             document.select("span.statusseries").text().trim()
         )
         val type = getType(document.select("span.typeseries").text().trim().lowercase())
-        val rating = document.select("span.nilaiseries").text().trim().toIntOrNull()
+        val rating = document.select("span.nilaiseries").text().trim().toDoubleOrNull()
         val description = document.select(".entry-content.seriesdesc > p").text().trim()
         val trailer = document.selectFirst("a.trailerbutton")?.attr("href")
 
@@ -132,17 +135,17 @@ class NontonAnimeID : MainAPI() {
             ).select("li").map {
                 val episode = Regex("Episode\\s?(\\d+)").find(
                     it.selectFirst("a")?.text().toString()
-                )?.groupValues?.getOrNull(0) ?: it.selectFirst("a")?.text()
+                )?.groupValues?.getOrNull(1)?.toIntOrNull()
                 val link = fixUrl(it.selectFirst("a")!!.attr("href"))
-                newEpisode(link) { this.episode = episode?.toIntOrNull() }
+                newEpisode(link) { this.episode = episode }
             }.reversed()
         } else {
             document.select("ul.misha_posts_wrap2 > li").map {
                 val episode = Regex("Episode\\s?(\\d+)").find(
                     it.selectFirst("a")?.text().toString()
-                )?.groupValues?.getOrNull(0) ?: it.selectFirst("a")?.text()
+                )?.groupValues?.getOrNull(1)?.toIntOrNull()
                 val link = it.select("a").attr("href")
-                newEpisode(link) { this.episode = episode?.toIntOrNull() }
+                newEpisode(link) { this.episode = episode }
             }.reversed()
         }
 
@@ -165,7 +168,7 @@ class NontonAnimeID : MainAPI() {
             this.year = year
             addEpisodes(DubStatus.Subbed, episodes)
             showStatus = status
-            this.score = rating?.toDouble()
+            this.score = rating?.let { Score(it) }
             plot = description
             addTrailer(trailer)
             this.tags = tags
@@ -181,23 +184,22 @@ class NontonAnimeID : MainAPI() {
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
     ): Boolean {
-
         val document = app.get(data).document
 
-        val nonce =
-            document.select("script#ajax_video-js-extra").attr("src").substringAfter("base64,")
-                .let { Regex("nonce\":\"(\\S+?)\"").find(base64Decode(it))?.groupValues?.get(1) }
+        val nonce = document.select("script#ajax_video-js-extra").attr("src")
+            .substringAfter("base64,")
+            .let { Regex("nonce\":\"(\\S+?)\"").find(base64Decode(it))?.groupValues?.get(1) }
 
-        document.select(".container1 > ul > li:not(.boxtab)").parallelMap {
-            val dataPost = it.attr("data-post")
-            val dataNume = it.attr("data-nume")
-            val serverName = it.attr("data-type").lowercase()
+        document.select(".container1 > ul > li:not(.boxtab)").parallelMap { element ->
+            val dataPost = element.attr("data-post")
+            val dataNume = element.attr("data-nume")
+            val serverName = element.attr("data-type").lowercase()
 
             val iframe = app.post(
                 url = "$mainUrl/wp-admin/admin-ajax.php",
                 data = mapOf(
                     "action" to "player_ajax",
-                    "nonce" to "$nonce",
+                    "nonce" to (nonce ?: ""),
                     "serverName" to serverName,
                     "nume" to dataNume,
                     "post" to dataPost,
@@ -205,11 +207,16 @@ class NontonAnimeID : MainAPI() {
                 referer = data,
                 headers = mapOf("X-Requested-With" to "XMLHttpRequest")
             ).document.selectFirst("iframe")?.attr("src")?.let {
-                if (it.contains("/video-frame/")) app.get(it).document.select("iframe")
-                    .attr("data-src") else it
+                if (it.contains("/video-frame/"))
+                    app.get(it).document.select("iframe").attr("data-src")
+                else it
             }
 
-            loadExtractor(iframe ?: return@parallelMap, "$mainUrl/", subtitleCallback, callback)
+            if (iframe != null) {
+                runBlocking {
+                    loadExtractor(iframe, mainUrl, subtitleCallback, callback)
+                }
+            }
         }
 
         return true
