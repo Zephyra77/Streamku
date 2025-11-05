7a22f6f6ce8fd4b74ae9697e4b1b64cd72abbb7d
Zephyra <dibomaharani18@gmail.com>
2025-11-05 07:33:24 +0700

    .

--- NontonAnimeID/src/main/kotlin/com/nontonanimeid/NontonAnimeID.kt
+++ NontonAnimeID/src/main/kotlin/com/nontonanimeid/NontonAnimeID.kt
@@ -159,7 +159,7 @@ class NontonAnimeID : MainAPI() {
             this.year = year
             addEpisodes(DubStatus.Subbed, episodes)
             showStatus = status
-            this.score = rating
+            this.score = rating?.let { Score.fromDouble(it) }
             plot = description
             addTrailer(trailer)
             this.tags = tags
