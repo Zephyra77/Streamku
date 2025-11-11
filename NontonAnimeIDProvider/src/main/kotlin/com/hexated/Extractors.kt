5de70679d590b53cd775c2f0db87cd80cde4038d
Zephyra <dibomaharani18@gmail.com>
2025-11-11 07:17:44 +0700

    .

--- NontonAnimeIDProvider/src/main/kotlin/com/hexated/Extractors.kt
+++ NontonAnimeIDProvider/src/main/kotlin/com/hexated/Extractors.kt
@@ -1,12 +1,22 @@
 package com.hexated
 
-import com.lagradost.cloudstream3.*
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.lagradost.cloudstream3.APIHolder
+import com.lagradost.cloudstream3.SubtitleFile
+import com.lagradost.cloudstream3.app
+import com.lagradost.cloudstream3.extractors.Filesim
+import com.lagradost.cloudstream3.extractors.Hxfile
+import com.lagradost.cloudstream3.utils.ExtractorApi
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.ExtractorLinkType
+import com.lagradost.cloudstream3.utils.Qualities
+import com.lagradost.cloudstream3.utils.newExtractorLink
 import com.lagradost.cloudstream3.utils.*
 
-class Nontonanimeid : ExtractorApi() {
-    override val name = "Nontonanimeid"
-    override val mainUrl = "https://nontonanimeid.boats"
-    override val requiresReferer = false
+open class Gdplayer : ExtractorApi() {
+    override val name = "Gdplayer"
+    override val mainUrl = "https://gdplayer.to"
+    override val requiresReferer = true
 
     override suspend fun getUrl(
         url: String,
@@ -14,15 +24,57 @@ class Nontonanimeid : ExtractorApi() {
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
     ) {
-        val doc = app.get(url, referer = referer).document
-        val iframe = doc.selectFirst("iframe[data-src*=\"kotakanimeid\"]")?.attr("data-src") ?: return
-        KotakAnimeidLink().getUrl(iframe, referer ?: mainUrl, subtitleCallback, callback)
+        val res = app.get(url, referer = referer).document
+        val script = res.selectFirst("script:containsData(kaken =)")?.data()
+        val kaken = script?.substringAfter("kaken = \"")?.substringBefore("\"") ?: return
+
+        val json = app.get(
+            "$mainUrl/api/?$kaken=&_=${APIHolder.unixTimeMS}",
+            headers = mapOf("X-Requested-With" to "XMLHttpRequest")
+        ).parsedSafe<Response>() ?: return
+
+        json.sources?.forEach { src ->
+            val file = src.file ?: return@forEach
+            callback.invoke(
+                newExtractorLink(name, name, file, ExtractorLinkType.VIDEO) {
+                    quality = getQuality(json.title)
+                }
+            )
+        }
+    }
+
+    private fun getQuality(str: String?): Int {
+        return Regex("(\\d{3,4})[pP]").find(str ?: "")
+            ?.groupValues?.getOrNull(1)?.toIntOrNull()
+            ?: Qualities.Unknown.value
+    }
+
+    data class Response(
+        @JsonProperty("title") val title: String? = null,
+        @JsonProperty("sources") val sources: ArrayList<Source>? = null
+    ) {
+        data class Source(
+            @JsonProperty("file") val file: String? = null,
+            @JsonProperty("type") val type: String? = null
+        )
     }
 }
 
-class KotakAnimeidLink : ExtractorApi() {
+class Nontonanimeid : Hxfile() {
+    override val name = "Nontonanimeid"
+    override val mainUrl = "https://nontonanimeid.boats"
+    override val requiresReferer = true
+}
+
+class EmbedKotakAnimeid : Hxfile() {
+    override val name = "EmbedKotakAnimeid"
+    override val mainUrl = "https://embed.kotakanimeid.link"
+    override val requiresReferer = true
+}
+
+class KotakAnimeidLink : Hxfile() {
     override val name = "KotakAnimeid"
-    override val mainUrl = "https://s1.kotakanimeid.link"
+    override val mainUrl = "https://kotakanimeid.link"
     override val requiresReferer = false
 
     override suspend fun getUrl(
@@ -34,17 +86,59 @@ class KotakAnimeidLink : ExtractorApi() {
         val res = app.get(url, referer = referer).document
         val script = res.selectFirst("script:containsData(sources:)")?.data() ?: return
         val m3u8 = Regex("\"file\"\\s*:\\s*\"(https[^\"]+\\.m3u8)\"")
-            .find(script)?.groupValues?.getOrNull(1) ?: return
+            .find(script)?.groupValues?.getOrNull(1)
 
-        callback.invoke(
-            ExtractorLink(
-                name,
-                name,
-                m3u8,
-                referer ?: mainUrl,
-                quality = Qualities.Unknown.value,
-                isM3u8 = true
+        if (!m3u8.isNullOrBlank()) {
+            callback.invoke(
+                newExtractorLink(name, name, m3u8, ExtractorLinkType.M3U8) {
+                    quality = Qualities.Unknown.value
+                }
             )
-        )
+        }
     }
+}
+
+class KotakAnimeidCom : Hxfile() {
+    override val name = "KotakAnimeidCom"
+    override val mainUrl = "https://kotakanimeid.com"
+    override val requiresReferer = true
+
+    override suspend fun getUrl(
+        url: String,
+        referer: String?,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ) {
+        val headers = mapOf(
+            "Referer" to (referer ?: mainUrl),
+            "Origin" to mainUrl,
+            "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+        )
+
+        val res = app.get(url, headers = headers).document
+        val script = res.selectFirst("script:containsData(sources:)")?.data() ?: return
+        val m3u8 = Regex("\"file\"\\s*:\\s*\"(https[^\"]+\\.m3u8)\"")
+            .find(script)?.groupValues?.getOrNull(1)
+
+        if (!m3u8.isNullOrBlank()) {
+            callback.invoke(
+                newExtractorLink(name, name, m3u8, ExtractorLinkType.M3U8) {
+                    quality = Qualities.Unknown.value
+                }
+            )
+        } else {
+            throw Exception("Tidak Ada Tautan Yang Ditemukan")
+        }
+    }
+}
+
+class Kotaksb : Hxfile() {
+    override val name = "Kotaksb"
+    override val mainUrl = "https://kotaksb.pro"
+    override val requiresReferer = true
+}
+
+class Vidhidepre : Filesim() {
+    override val name = "Vidhidepre"
+    override var mainUrl = "https://vidhidepre.com"
 }
